5265ef835e208f65d05f130e0197cd6503c35422
Huu Khai NGUYEN (luciffer1904) <khainguyenhuu121@gmail.com>
2023-05-08 15:16:28 +0200

    Change filename

--- 01-practice/app.js
+++ 01-practice/app.js
@@ -1,5 +1,5 @@
 const http = require("http")
-const routers = require("./routers")
+const routers = require("./routes")
 
 const server = http.createServer(routers.requestHandler)
 server.listen(3000, () => console.log("Listening on port 3000"))
